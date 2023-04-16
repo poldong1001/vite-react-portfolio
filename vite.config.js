4fa29ccd4f20751e507fa826765e1a0ecec8a731
poldong1001 <poldong2019@gmail.com>
2023-04-16 21:08:27 +0800

    adding dist two

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vite-project/",
   plugins: [react()],
-  base: "/mlb/",
+  // base: "/mlb/",
 })
